44046e8cbbefc774ae136a33bd4e8b4e627f9460
MrSullivan <>
2021-05-30 21:58:14 +0000

    lots

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,18 @@
 def main():
     #write your code below this line
+    import csv
+    nameOfFile = input("Name of the file:")
+    
+    with open(nameOfFile) as f:
+      reader = csv.reader(f,delimiter=',')
+      for row in reader:
+        if(str(1) in row[1]):
+          print(row[0] + ", age: " + row[1] + " year")
+        else:
+          print(row[0] + ", age: " + row[1] + " years")
+        
+    
+
 
 if __name__ == '__main__':
     main()
